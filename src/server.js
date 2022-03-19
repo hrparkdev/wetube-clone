037b260c3a5fa235d2a6b499c036986ea8841866
hrparkdev <hrparkdev@gmail.com>
2022-03-19 22:42:16 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import "./db";
+import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -8,8 +10,11 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
