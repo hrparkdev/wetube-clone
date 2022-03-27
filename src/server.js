549fb57b67ae35a573e364905d35dbb280d92ac1
hrparkdev <hrparkdev@gmail.com>
2022-03-27 11:32:51 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import {
+  localsMiddleware
+} from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -10,7 +14,19 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
