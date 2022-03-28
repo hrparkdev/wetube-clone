3b3e10911e1cdad6f1627c016ba65929e8c0457b
hrparkdev <hrparkdev@gmail.com>
2022-03-28 23:39:41 +0900

    Update middlewares.js

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,5 @@
 export const localsMiddleware = (req, res, next) => {
-  if (req.session.loggedIn) {
-    res.locals.loggedIn = true;
-  }
+  res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user;
   next();
