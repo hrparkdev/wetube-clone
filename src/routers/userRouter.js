19f7897b01c8098bd473909e5c8854720e9c7750
hrparkdev <hrparkdev@gmail.com>
2022-04-02 23:18:29 +0900

    Add extension

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { logout, edit, remove, see, startGithubLogin, finishGithubLogin } from "../controllers/userController";
+import { logout, edit, see, startGithubLogin, finishGithubLogin } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", see);
