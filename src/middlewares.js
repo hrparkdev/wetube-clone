09bc2c76c3a0ce140678e4ce3dd80311a30e9789
hrparkdev <hrparkdev@gmail.com>
2022-04-06 22:08:28 +0900

    Add protectorMiddleWare

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,3 +4,11 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedInUser = req.session.user || {};
   next();
 };
+
+export const protectorMiddleWare = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    return res.redirect("/login");
+  }
+};
