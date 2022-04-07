3ccf94e1419e1d61db54963cef68cd670a384f59
hrparkdev <hrparkdev@gmail.com>
2022-04-07 23:04:49 +0900

    Add publicOnlyMiddleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -12,3 +12,11 @@ export const protectorMiddleWare = (req, res, next) => {
     return res.redirect("/login");
   }
 };
+
+export const publicOnlyMiddleware = (req, res next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};
