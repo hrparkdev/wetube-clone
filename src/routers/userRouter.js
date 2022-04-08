057e91f3739ccbe46b2bc2037ba97f55a9f591ed
hrparkdev <hrparkdev@gmail.com>
2022-04-08 22:25:28 +0900

    Update userRouter.js

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { logout, getEdit, postEdit, see, startGithubLogin, finishGithubLogin } from "../controllers/userController.js";
+import { protectorMiddleWare } from "../middleware";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.route("/edit").get(getEdit).post(postEdit);
+userRouter.get("/logout", protectorMiddleWare, logout);
+userRouter.route("/edit").all(protectorMiddleWare).get(getEdit).post(postEdit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", see);
