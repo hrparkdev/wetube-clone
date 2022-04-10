7829eb3f59b3adda57f1e364861065a34e5a14e3
hrparkdev <hrparkdev@gmail.com>
2022-04-10 13:02:32 +0900

    Update videoRouter.js

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { watch, getEdit, postEdit, getUpload, postUpload, deleteVideo } from "../controllers/videoController.js";
+import { protectorMiddleWare } from "../middlewares.js";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter.route("/:id([0-9a-f]{24})/edit").all(protectorMiddleWare).get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").all(protectorMiddleWare).get(deleteVideo);
+videoRouter.route("/upload").all(protectorMiddleWare).get(getUpload).post(postUpload);
 
 export default videoRouter;
