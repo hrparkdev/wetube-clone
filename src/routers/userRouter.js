936277cf234433a29459f7143c3e3a3372ed9197
hrparkdev <hrparkdev@gmail.com>
2022-04-19 23:54:12 +0900

    Update userRouter.js

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,4 @@
 import express from "express";
-// import { all } from "express/lib/application";
 import {
   logout,
   getEdit,
@@ -10,15 +9,23 @@ import {
   postChangePassword,
   see
 } from "../controllers/userController.js";
-import { protectorMiddleWare, publicOnlyMiddleware } from "../middlewares.js";
+import { protectorMiddleWare, publicOnlyMiddleware, videoUpload } from "../middlewares.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleWare, logout);
-userRouter.route("/edit").all(protectorMiddleWare).get(getEdit).post(postEdit);
+userRouter
+  .route("/edit")
+  .all(protectorMiddleWare)
+  .get(getEdit)
+  .post(videoUpload.single("avatar"), postEdit);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
-userRouter.route("/change-password").all(protectorMiddleWare).get(getChangePassword).post(postChangePassword);
+userRouter
+  .route("/change-password")
+  .all(protectorMiddleWare)
+  .get(getChangePassword)
+  .post(postChangePassword);
 userRouter.get("/:id", see);
 
 export default userRouter;
